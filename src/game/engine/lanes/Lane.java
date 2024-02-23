b67ed7b9db54269a3401f3c0b7d61b409a1a929a
Ahmed1Elsayed <ahmed.m.elsayed20@gmail.com>
2024-02-23 19:58:39 +0200

    I built the lane class

---
+++ src/game/engine/lanes/Lane.java
@@ -0,0 +1,44 @@
+package game.engine.lanes;
+
+import game.engine.base.Wall;
+import game.engine.titans.Titan;
+import game.engine.weapons.Weapon;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Lane implements Comparable <Lane> {
+    private final Wall laneWall;
+    private int dangerLevel;
+    private final PriorityQueue<Titan> titans;
+    private final ArrayList<Weapon> weapons;
+    public Lane(Wall laneWall){
+
+        this.laneWall=laneWall;
+        this.dangerLevel=0;
+        this.titans=new PriorityQueue<Titan>();
+        this.weapons=new ArrayList<Weapon>();
+    }
+   public int compareTo(Lane o){
+        return this.dangerLevel-o.dangerLevel;
+   }
+
+    public int getDangerLevel() {
+        return dangerLevel;
+    }
+
+    public void setDangerLevel(int dangerLevel) {
+        this.dangerLevel = dangerLevel;
+    }
+
+    public PriorityQueue<Titan> getTitans() {
+        return titans;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public Wall getLaneWall() {
+        return laneWall;
+    }
+}
