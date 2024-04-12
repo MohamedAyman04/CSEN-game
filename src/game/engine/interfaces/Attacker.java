c33218aa06a7868f102307cede5e25fd005ed266
Ahmed1Elsayed <ahmed.m.elsayed20@gmail.com>
2024-04-12 17:36:38 +0200

    finished the Attacker interface

--- src/game/engine/interfaces/Attacker.java
+++ src/game/engine/interfaces/Attacker.java
@@ -4,6 +4,13 @@ public interface Attacker
 {
 	int getDamage(); // gets the damage value to be applied
 
-
+	default int attack(Attackee target) {
+		target.takeDamage(getDamage());
+		if (target.isDefeated()) {
+			return target.getResourcesValue();
+		}
+		else
+			return 0;
+	}
 
 }
